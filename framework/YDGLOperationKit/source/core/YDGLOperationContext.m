587fb71be73e316e020a2a9b21d36b069b72ed9a
9527 <huizexu@gmail.com>
2016-04-14 18:19:33 +0800

    内容:完善YDGLOperationContext

--- framework/YDGLOperationKit/source/core/YDGLOperationContext.m
+++ framework/YDGLOperationKit/source/core/YDGLOperationContext.m
@@ -12,52 +12,63 @@
 
 @implementation YDGLOperationContext
 
-NSMutableOrderedSet<EAGLContext*>* contexts;
+static NSMutableOrderedSet<EAGLContext*>* contexts;
+
+static dispatch_semaphore_t s_lock;
+
 
 +(void)initialize{
-
+    
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         
         contexts=[NSMutableOrderedSet orderedSet];
         
+        s_lock=dispatch_semaphore_create(1);
+        
     });
-
+    
 }
 
 +(void)pushContext{
     
     EAGLRenderingAPI api = kEAGLRenderingAPIOpenGLES2;
-    static dispatch_semaphore_t locker;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        
-        locker=dispatch_semaphore_create(1);
-        
-    });
     
     EAGLContext *instance;
     
-    dispatch_semaphore_wait(locker,DISPATCH_TIME_FOREVER);
-    
     instance=[[EAGLContext alloc]initWithAPI:api];
     
-    dispatch_semaphore_signal(locker);
-    
+    dispatch_semaphore_wait(s_lock,DISPATCH_TIME_FOREVER);
     
     [contexts addObject:instance];
+    
+    dispatch_semaphore_signal(s_lock);
+    
     
 }
 
 +(void)popContext{
-
+    
+    dispatch_semaphore_wait(s_lock, DISPATCH_TIME_FOREVER);
+    
     [contexts removeObjectAtIndex:contexts.count-1];
-
+    
+    dispatch_semaphore_signal(s_lock);
+    
+    
 }
 
 +(EAGLContext *)currentGLContext{
-
-    return [contexts lastObject];//使用instance的话,需要调用glFlush()
+    
+    EAGLContext *instance;
+    
+    dispatch_semaphore_wait(s_lock,DISPATCH_TIME_FOREVER);
+    
+    instance=[contexts lastObject];
+    
+    dispatch_semaphore_signal(s_lock);
+    
+    return instance;
     
 }
 
